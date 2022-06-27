4dd30cec17bef4f37aba940c95041a4c18959885
Filip Tijan <ftijan@gmail.com>
2022-06-27 22:16:49 +0100

    manual routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import Navbar from "./Navbar";
+import Home from "./pages/Home";
+import Pricing from "./pages/Pricing";
+import About from "./pages/About";
 
 function App() {
+  let component;
+  console.log(window.location);
+  switch (window.location.pathname) {
+    case "/":
+      component = <Home />;
+      break;
+    case "/pricing":
+      component = <Pricing />;
+      break;
+    case "/about":
+      component = <About />;
+      break;
+  }
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <div className="container">
+        {component}
+      </div>      
+    </>
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,22 @@
 export default function Navbar() {
+    const path = window.location.pathname;
     return <nav className="nav">
+        
         <a href="/" className="site-title">
             Site Name
         </a>
-        <ul>            
-            <li className="active">
-                <a href="/pricing">Pricing</a>
-            </li>
-            <li>
-                <a href="/about">About</a>
-            </li>
+        <ul>
+            <CustomLink href="/pricing">Pricing</CustomLink>            
+            <CustomLink href="/about">About</CustomLink>            
         </ul>
     </nav>
+}
+
+function CustomLink({ href, children, ...props }) {
+    const path = window.location.pathname;
+    return (
+        <li className={path === href ? "active" : ""}>
+            <a href={href} {...props}>{children}</a>
+        </li>
+    )
 }
