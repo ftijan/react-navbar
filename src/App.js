72f23c0a78093bdc704c5ad3ea2acbed73e95959
Filip Tijan <ftijan@gmail.com>
2022-06-27 22:30:32 +0100

    routing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,26 +2,19 @@ import Navbar from "./Navbar";
 import Home from "./pages/Home";
 import Pricing from "./pages/Pricing";
 import About from "./pages/About";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  let component;
-  console.log(window.location);
-  switch (window.location.pathname) {
-    case "/":
-      component = <Home />;
-      break;
-    case "/pricing":
-      component = <Pricing />;
-      break;
-    case "/about":
-      component = <About />;
-      break;
-  }
+  
   return (
     <>
       <Navbar />
       <div className="container">
-        {component}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
       </div>      
     </>
   );
